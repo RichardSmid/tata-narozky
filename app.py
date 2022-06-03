dfe83cd1a44654fc0181d7636bc696cc87bdce4c
Ondym <ondramat007@gmail.com>
2022-06-03 22:51:17 +0200

    komytík

--- app.py
+++ app.py
@@ -82,4 +82,4 @@ def update(id):
         return render_template('update.html')
 
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", debug=True)
+    app.run(host="0.0.0.0", port=80, debug=True)
